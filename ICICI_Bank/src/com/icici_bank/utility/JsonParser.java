aeae017f3afbbf49ec6e6df323b50b733feff1b1
Ishwar Mohan <107830130+Ishwar-Mohan@users.noreply.github.com>
2024-02-11 13:30:37 +0530

    Update JsonParser.java

--- ICICI_Bank/src/com/icici_bank/utility/JsonParser.java
+++ ICICI_Bank/src/com/icici_bank/utility/JsonParser.java
@@ -2,8 +2,8 @@ package com.icici_bank.utility;
 
 import com.google.gson.Gson;
 
-public class JsonParser {
-	
+public class JsonParser 
+{
 	public static String getJsonString(Object o)
 	{
 		return new Gson().toJson(o);
